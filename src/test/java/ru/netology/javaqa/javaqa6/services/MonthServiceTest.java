0edd1b2026c93b88a7741e4c94af5d3437d9b61a
Aleksandr <126883049+AleksandrMuzhev@users.noreply.github.com>
2023-04-18 12:01:40 +0500

    Retry Update MonthServiceTest

--- src/test/java/ru/netology/javaqa/javaqa6/services/MonthServiceTest.java
+++ src/test/java/ru/netology/javaqa/javaqa6/services/MonthServiceTest.java
@@ -1,10 +1,10 @@
 package ru.netology.javaqa.javaqa6.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
+// import org.junit.jupiter.params.provider.CsvSource;
 
 public class MonthServiceTest {
 
